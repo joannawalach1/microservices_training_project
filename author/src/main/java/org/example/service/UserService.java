1f3e0de6a6f9d36e09a4ca6ca08784f7624baaf8
joannawalach1 <joanna.walach@op.pl>
2024-02-20 22:34:45 +0100

    impl password encoder

--- author/src/main/java/org/example/service/UserService.java
+++ author/src/main/java/org/example/service/UserService.java
@@ -3,14 +3,17 @@ package org.example.service;
 import lombok.RequiredArgsConstructor;
 import org.example.entity.User;
 import org.example.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-private UserRepository userRepository;
+private final UserRepository userRepository;
+private final PasswordEncoder passwordEncoder;
 
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));;
         return userRepository.saveAndFlush(user);
     }
 }
